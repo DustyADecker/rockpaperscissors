cb7cd92a6e9f054efd71bc0035e1b0db9891aa53
DustyADecker <dustyadecker@gmail.com>
2025-01-29 19:06:10 -0600

    Pseudocode potential solution for computer choice

---
+++ javascript.js
@@ -0,0 +1,24 @@
+function getComputerChoice {
+
+}
+
+/* 
+we need the computer to randomly return one of the three
+strings "rock", "paper" or "scissors"
+
+create function
+select one of the three strings "rock", "paper" or "scissors"
+return the selected string
+
+potential method:
+create function
+initialize variable number
+use math.random to generate a random number
+code if/else if/else statements so that:
+    if number < 0.3 return "rock"
+    else if 0.3 < number < 0.6 return "paper"
+    else return "scissors"
+return string
+
+
+*/
