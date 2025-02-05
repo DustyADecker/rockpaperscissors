a766bca5a0e52d9c9edfb4e5f5aa1703c939d0c1
DustyADecker <dustyadecker@gmail.com>
2025-02-05 15:18:22 -0600

    Add javascript2.js and get working if/else syntax

--- javascript.js
+++ javascript.js
@@ -1,24 +1,11 @@
-function getComputerChoice {
-    let number = Math.random;
-}
-
-/* 
-we need the computer to randomly return one of the three
-strings "rock", "paper" or "scissors"
-
-create function
-select one of the three strings "rock", "paper" or "scissors"
-return the selected string
-
-potential method:
-create function
-initialize variable number
-use math.random to generate a random number
-code if/else if/else statements so that:
-    if number < 0.3 return "rock"
-    else if 0.3 < number < 0.6 return "paper"
-    else return "scissors"
-return string
-
-
-*/
+let random = Math.random();
+let result;
+      console.log(random);
+      if (Math.random < 0.3) {
+        result = "rock";
+    } else if (Math.random < 0.6) {
+        result = "paper";
+    } else 
+    return random;
+    console.log(result);
+    
---
+++ javascript2.js
@@ -0,0 +1,22 @@
+/*
+let randomNumber = Math.random();
+console.log(randomNumber);
+let result;
+if (randomNumber < 0.3) {
+    result = "rock"
+} else {
+    result = "paper"
+}
+return result;
+}
+*/
+
+let number = Math.random;
+let result;
+
+if (number < 0.5) {
+    result = "Number is less than 0.5."
+} else {
+    result = "Number is not less than 0.5."
+}
+alert(result);
