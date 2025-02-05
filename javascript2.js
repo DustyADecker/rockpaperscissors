e5eb41477e24b3765c0b9e9df3e9a40d6085e277
DustyADecker <dustyadecker@gmail.com>
2025-02-05 16:33:05 -0600

    Find working solution for function 'getComputerChoice'

--- javascript2.js
+++ javascript2.js
@@ -1,25 +1,12 @@
-/*
-let randomNumber = Math.random();
-console.log(randomNumber);
-let result;
-if (randomNumber < 0.3) {
-    result = "rock"
-} else {
-    result = "paper"
-}
-return result;
-}
-*/
-
-let number = Math.random();
-let result;
-console.log(number);
-if (number >= 0 && number <= 0.33) {
-    result = "Rock!";
-} else if (number > 0.33 && number <= 0.66) {
-    result = "Paper!";
-} else {
-    result = "Scissors!"
-}
-alert(result);
-
+function getComputerChoice() {
+    let randomNumber = Math.random(); // Generates a number between 0 (inclusive) and 1 (exclusive)
+    console.log(randomNumber);
+    if (randomNumber >= 0 && randomNumber <= 0.33) {
+      return "Rock!";
+    } else if (randomNumber > 0.33 && randomNumber <= 0.66) {
+      return "Paper!";
+    } else if (randomNumber > 0.66){
+        return "Scissors!"
+    }
+  }
+  alert(getComputerChoice());
